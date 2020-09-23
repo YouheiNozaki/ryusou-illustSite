99a6330d385b90cecf8d005ebb66d154e59701e3
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-09-23 21:58:25 +0900

    setting eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
